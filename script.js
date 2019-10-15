dc5d06bc527ee9a105887942e0b0bdfc326e9ed0
shownola <swasieko@gmail.com>
2019-10-15 15:04:17 -0700

    Complete Gallery section using javascript to change classes and start customer section

--- script.js
+++ script.js
@@ -20,5 +20,17 @@ $(document).ready(function(){
       $('.camera-img').removeClass('fromLeft');
       $('.mission-text').removeClass('fromRight');
     }
+  });
+  $('.gallery-list-item').click(function(){
+    let value = $(this).attr('data-filter');
+    if(value === 'all'){
+      $('.filter').show(300);
+    } else {
+      $('.filter').not('.' + value).hide(300);
+      $('.filter').filter('.' + value).show(300);
+    }
+  });
+  $('.gallery-list-item').click(function(){
+    $(this).addClass('active-item').siblings().removeClass('.active-item');
   })
 });
